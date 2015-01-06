f0cdd9392cc89db3e2112cc8a0e67c4f1c519817
gerbil <jurijs.kobecs@gmail.com>
2015-01-06 13:53:00 +0200

    'ololo'

--- app/alarms/scripts/ServerListController.js
+++ app/alarms/scripts/ServerListController.js
@@ -2,16 +2,29 @@ angular
     .module('alarms')
     .factory('Alarms', function ($resource) {
         return $resource(
-            'http://10.30.60.126:3000/alarms/:server/:action/:id',
+            'http://localhost:3000/alarms/:server/:action/:id',
             {server: '@server'},
             {
-                list: {isArray: false},
-                add: {method: 'POST', params: {action: 'add'}},
-                delete: {method: 'POST', params: {action: 'delete', id: '@id'}},
-                update: {method: 'POST', params: {action: 'update', id: '@id'}}
+                list: {
+                    method: 'GET',
+                    isArray: false
+                },
+                add: {
+                    method: 'POST',
+                    params: {action: 'add'}
+                },
+                delete: {
+                    method: 'POST',
+                    params: {action: 'delete', id: '@id'}
+                },
+                update: {
+                    method: 'POST',
+                    params: {action: 'update', id: '@id'}
+                }
             }
         );
     })
+
     .controller('ServerListController', function ($scope, supersonic, $resource, Alarms) {
 
         $scope.viewTitle = 'Server list';
@@ -19,7 +32,7 @@ angular
         steroids.on('ready', supersonic.ui.navigationBar.hide());
 
         // Get all servers from backend alarms factory API
-        Alarms.list(function (data) {
+        Alarms.list({apikey: 'asdasjsdgfjkjhg'}, function (data) {
             $scope.serverList = data.servers;
         });
 
@@ -28,11 +41,11 @@ angular
         };
 
         $scope.openAlarmsList = function (server) {
-            var AlarmsList = new steroids.views.WebView('app/alarms/alarmsList.html?server='+server);
+            var AlarmsList = new steroids.views.WebView('app/alarms/alarmsList.html?server=' + server);
             steroids.layers.push(AlarmsList);
         };
 
-        var webView = new steroids.views.WebView({location: "app/alarms/alarmsList.html?server=test", id:"testAlarmsView"});
+        var webView = new steroids.views.WebView({location: "app/alarms/alarmsList.html?server=test", id: "testAlarmsView"});
         webView.preload();
 
 
